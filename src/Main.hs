03cceff07d0d0bfe3a03bc2a57030f720bd04a76
Michael Hueschen <michaelhueschen@gmail.com>
2017-01-12 12:23:38 -0800

    Runs but MIDI cannot find output device

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,15 @@
 module Main where
 
-import Euterpea.Music.Note.Music
+import Euterpea.Music
+import Euterpea.IO.MIDI.Play
 
 main :: IO ()
 main = do
   putStrLn "hello world"
+  play t251
+
+t251 :: Music Pitch
+t251 = let dMinor = d 4 wn :=: f 4 wn :=: a 4 wn
+           gMajor = g 4 wn :=: b 4 wn :=: d 5 wn
+           cMajor = c 4 bn :=: e 4 bn :=: g 4 bn
+       in  dMinor :+: gMajor :+: cMajor
