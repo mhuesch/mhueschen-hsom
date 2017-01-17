805799cad1ab4415d901021f85480258647cc7e1
Michael Hueschen <michaelhueschen@gmail.com>
2017-01-16 17:10:32 -0800

    To Build A Home intro

--- src/Main.hs
+++ src/Main.hs
@@ -5,9 +5,20 @@ import Euterpea.IO.MIDI.Play
 
 main :: IO ()
 main = do
-  putStrLn "Is t251 == (twoFiveOne (D, 4) wn) ?"
-  putStrLn $ answer (t251 == (twoFiveOne (D, 4) wn))
-  play t251
+--  putStrLn "Is t251 == (twoFiveOne (D, 4) wn) ?"
+--  putStrLn $ answer (t251 == (twoFiveOne (D, 4) wn))
+--  play t251
+  play toBuildAHomeIntro
+
+
+toBuildAHomeIntro :: Music Pitch
+toBuildAHomeIntro =
+  let dur = wn
+      dMajor = majorTriadMidUp (D, 4) dur
+      aMajor = majorTriadMidUp (A, 3) dur
+      eSecond = secondTriadMidUp (E, 3) (dur/8)
+      eMajor = majorTriadMidUp (E, 3) dur
+  in  line [dMajor, aMajor, eSecond, eMajor]
 
 t251 :: Music Pitch
 t251 = let dMinor = d 4 wn :=: f 4 wn :=: a 4 wn
@@ -21,6 +32,16 @@ twoFiveOne p d =
       chord_V  = majorTriad (trans 5 p) d
       chord_I  = majorTriad (trans (-2) p) (2*d)
   in  chord_ii :+: chord_V :+: chord_I
+
+secondTriadMidUp :: Pitch -> Dur -> Music Pitch
+secondTriadMidUp p d = note d p
+                   :=: note d (trans (12+2) p)
+                   :=: note d (trans 7 p)
+
+majorTriadMidUp :: Pitch -> Dur -> Music Pitch
+majorTriadMidUp p d = note d p
+                  :=: note d (trans (12+4) p)
+                  :=: note d (trans 7 p)
 
 majorTriad :: Pitch -> Dur -> Music Pitch
 majorTriad p d = note d p
